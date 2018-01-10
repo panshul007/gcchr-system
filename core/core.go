8f7898cb6aace612b2b086dccb36ded0fae20a63
Panshul Gupta <panshul007@gmail.com>
2018-01-10 23:25:00 +0100

    update: load config.

--- core/core.go
+++ core/core.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gcchr-system/core/model"
 )
 
 func main() {
+
+	prodEnv := flag.Bool("prod", false, "Set to true to run the server in production mode. core.config is required if set to true.")
+
+	flag.Parse()
+	config := model.LoadConfig(*prodEnv)
+
 	fmt.Println("This is gcchr system core.")
+	fmt.Printf("%+v\n", config)
 	user := model.User{}
 	user.Create()
 }
