c816f5766fdb6b88464b2facf473177b7bfb65a3
Panshul Gupta <panshul007@gmail.com>
2018-01-21 00:04:07 +0100

    add: methods to parse template files and render a view.

---
+++ core/views/view.go
@@ -0,0 +1,113 @@
+package views
+
+import (
+	"bytes"
+	"errors"
+	"gcchr-system/core/context"
+	"html/template"
+	"io"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/csrf"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateDir string = "views/"
+	TemplateExt string = ".gohtml"
+)
+
+type View struct {
+	Template *template.Template
+	Layout   string
+}
+
+func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	v.Render(w, r, nil)
+}
+
+// Render is used to render the view with predefined layout.
+func (v *View) Render(w http.ResponseWriter, r *http.Request, data interface{}) {
+	w.Header().Set("Content-Type", "text/html")
+	var vd Data
+	switch d := data.(type) {
+	case Data:
+		vd = d
+		// do nothing
+	default:
+		vd = Data{
+			Yield: data,
+		}
+	}
+
+	if alert := getAlert(r); alert != nil && vd.Alert == nil {
+		vd.Alert = alert
+		clearAlert(w)
+	}
+
+	vd.User = context.User(r.Context())
+	var buf bytes.Buffer
+
+	// CSRF
+	csrfField := csrf.TemplateField(r)
+	tmplt := v.Template.Funcs(template.FuncMap{
+		"csrfField": func() template.HTML {
+			return csrfField
+		},
+	})
+
+	if err := tmplt.ExecuteTemplate(&buf, v.Layout, vd); err != nil {
+		http.Error(w, AlertMessageGeneric, http.StatusInternalServerError)
+		return
+	}
+
+	// this throws an error, we really have no way to recover from this error, so it is not required here
+	// to check and handle the error. We will let it panic.
+	io.Copy(w, &buf)
+
+}
+
+func NewView(layout string, files ...string) *View {
+	addTemplatePath(files)
+	addTemplateExt(files)
+	files = append(files, layoutFiles()...)
+
+	t, err := template.New("").Funcs(template.FuncMap{
+		"csrfField": func() (template.HTML, error) {
+			return "", errors.New("csrfField is not implemented")
+		},
+	}).ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+
+	return &View{
+		Template: t,
+		Layout:   layout,
+	}
+}
+
+// layoutFiles Returns a slice of default layout files to be added to each view, eg: navbar, footer.
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+// addTemplatePath takes in slice of strings
+// representing file paths for templates, and it prepends
+// the TemplateDir directory to each string in the slice
+func addTemplatePath(files []string) {
+	for i, f := range files {
+		files[i] = TemplateDir + f
+	}
+}
+
+func addTemplateExt(files []string) {
+	for i, f := range files {
+		files[i] = f + TemplateExt
+	}
+}
