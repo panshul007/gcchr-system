f003136b99c6155be69f22e84d2617a0f90de208
Panshul Gupta <panshul007@gmail.com>
2018-01-21 01:03:48 +0100

    add: controller helpers to parse form values.

---
+++ core/controllers/helpers.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/gorilla/schema"
+)
+
+func parseForm(r *http.Request, dst interface{}) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+	return parseValues(r.PostForm, dst)
+}
+
+func parseURLParams(r *http.Request, dst interface{}) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+	return parseValues(r.Form, dst)
+}
+
+func parseValues(values url.Values, dst interface{}) error {
+	dec := schema.NewDecoder()
+	// Set to ignore CSRF fields, being submitted with every form.
+	dec.IgnoreUnknownKeys(true)
+	if err := dec.Decode(dst, values); err != nil {
+		return err
+	}
+	return nil
+}
